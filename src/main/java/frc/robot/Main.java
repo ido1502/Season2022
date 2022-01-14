c924c19f3fa508d85eee76f2241e47051154f898
ido1502 <ido.sevish@gmail.com>
2022-01-14 13:10:38 +0200

    delete Test

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -19,9 +19,6 @@ public final class Main {
    *
    * <p>If you change your main robot class, change the parameter type.
    */
-  public void test(){
-    return;
-  }
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
