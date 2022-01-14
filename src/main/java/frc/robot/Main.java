28fa09feb33abb55bde86773677cac95b7a0f764
ido1502 <ido.sevish@gmail.com>
2022-01-14 13:11:30 +0200

    some

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -19,6 +19,9 @@ public final class Main {
    *
    * <p>If you change your main robot class, change the parameter type.
    */
+  public static void some(){
+    
+  } 
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
